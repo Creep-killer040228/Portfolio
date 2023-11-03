cbf5d657d7512d09a36e47e73fc09e5388a8c2af
Samvel <samveld396@gmail.com>
2023-11-03 19:15:39 +0500

    fiks

--- src/stores/data.js
+++ src/stores/data.js
@@ -23,7 +23,6 @@ export const useData = defineStore({
     projectsAll: [
       { name: "VuePizza", desc: "Cамая вкусная пицца во вселенной", stak: "HTML SCSS VUE.JS", img: '1', url: 'vue-pizzaa.vercel.app' },
       { name: "Sneakers", desc: "Магазин лучших кроссовок", stak: "HTML SCSS VUE.JS", img: '3', url: 'sneakers-7ohu2kd4d-samvels-projects.vercel.app' },
-      { name: "Sneakers", desc: "Магазин лучших кроссовок", stak: "HTML SCSS VUE.JS", img: '3', url: 'sneakers-7ohu2kd4d-samvels-projects.vercel.app' },
     ],
     projectsSmall: [
       { name: "TodoList", desc: "Список заметок", stak: "HTML SCSS VUE JS", img: '4', url: 'creep-killer040228.github.io/TodoList/' },
